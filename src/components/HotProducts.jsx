2399d9f78658aba5b169e64ccf0075833ed49e1e
Ingrid <ingrid.chi77@gmail.com>
2024-12-30 01:29:05 +0800

    implement HotProduct

--- src/components/HotProducts.jsx
+++ src/components/HotProducts.jsx
@@ -1,12 +1,36 @@
 import { useState } from 'react';
 import styles from './HotProducts.module.scss';
+import hotImage_01 from '../assets/images/49851.jpg';
+import hotImage_02 from '../assets/images/49870.jpg';
+import hotImage_03 from '../assets/images/50049.jpg';
+import hotImage_04 from '../assets/images/50308.jpg';
 
 const HotProducts = () => {
   const products = [
-    { id: 1, name: '商品 1', description: '這是商品描述 1' },
-    { id: 2, name: '商品 2', description: '這是商品描述 2' },
-    { id: 3, name: '商品 3', description: '這是商品描述 3' },
-    { id: 4, name: '商品 4', description: '這是商品描述 4' },
+    {
+      id: 1,
+      name: '商品 1',
+      description: '這是商品描述 1',
+      src: hotImage_01,
+    },
+    {
+      id: 2,
+      name: '商品 2',
+      description: '這是商品描述 2',
+      src: hotImage_02,
+    },
+    {
+      id: 3,
+      name: '商品 3',
+      description: '這是商品描述 3',
+      src: hotImage_03,
+    },
+    {
+      id: 4,
+      name: '商品 4',
+      description: '這是商品描述 4',
+      src: hotImage_04,
+    },
   ];
 
   const [currentPage, setCurrentPage] = useState(0);
@@ -29,7 +53,9 @@ const HotProducts = () => {
       <div className={styles.grid}>
         {products.map((product) => (
           <div key={product.id} className={styles.product}>
-            <div className={styles.image}></div>
+            <div className={styles.image}>
+              <img src={product.src} />
+            </div>
             <div className={styles.name}>{product.name}</div>
             <div className={styles.description}>{product.description}</div>
           </div>
@@ -46,7 +72,9 @@ const HotProducts = () => {
             .slice(currentPage * 2, currentPage * 2 + 2)
             .map((product) => (
               <div key={product.id} className={styles.product}>
-                <div className={styles.image}></div>
+                <div className={styles.image}>
+                  <img src={product.src} />
+                </div>
                 <div className={styles.name}>{product.name}</div>
                 <div className={styles.description}>{product.description}</div>
               </div>
